e7f6f2418fa10d5e8285a1a1947dc11d9e9b22a9
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-26 09:45:00 -0600

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      title: 'QR Code Scanner',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
